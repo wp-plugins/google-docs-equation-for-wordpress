440881d323a6a75eb024b305b28dd9d73c6896e1
tigerlihao <tigerlihao@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-22 02:27:36 +0000

    add the basic files.
    git-svn-id: https://plugins.svn.wordpress.org/google-docs-equation-for-wordpress/trunk@185999 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ geq4wp.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Google Equation for WordPress
+Plugin URI: http://blog.tigerlihao.cn/geq4wp
+Description: Using Google chart API for LaTeX to add mathematical equations as images.
+Version: 0.1.0
+Author: Li Hao
+Author URI: http://tigerlihao.cn
+*/
+
+/*  
+    1.0.0:
+    Li Hao, URI: http://tigerlihao.cn
+    -   Li Hao's original version.
+*/
+
+class geq {
+	var $server = "http://www.google.com/chart?cht=tx&chf=bg,s,FFFFFF00&chco=000000&chl=";		
+	var $img_format = "png";
+	
+	// parsing the text to display tex by putting tex-images-tags into the code created by createTex
+    function parseTex ($toParse) {
+        // tag specification (which tags are to be replaced)
+        $regex = '#\[eq\](.*?)\[/eq\]#si';
+        
+		return preg_replace_callback($regex, array(&$this, 'createTex'), $toParse);
+    }
+    
+    // reading the tex-expression and create an image and a image-tag representing that expression
+    function createTex($toTex) {
+    	$equation_text = $toTex[1];
+		$equation_hash = md5($equation_text);
+		$equation_filename = 'eq_'.$equation_hash.'.'.$this->img_format;
+
+		$cache_path = ABSPATH . '/wp-content/uploads/';
+		$cache_equation_path = $cache_path . $equation_filename;
+		$cache_url = get_bloginfo('wpurl') . '/wp-content/uploads/';
+		$cache_equation_url = $cache_url . $equation_filename;
+    
+        if ( !is_file($cache_equation_path)) {
+     		if (!class_exists('Snoopy')) require_once (ABSPATH.'wp-includes/class-snoopy.php');
+     		
+            $snoopy = new Snoopy;
+			
+			$snoopy->fetch( $this->server.rawurlencode(html_entity_decode($equation_text)));
+            // this will copy the created tex-image to your cache-folder
+            if(strlen($snoopy->results)){
+				$cache_file = fopen($cache_equation_path, 'w');
+				fputs($cache_file, $snoopy->results);
+				fclose($cache_file);
+			}
+		}
+        
+        // returning the image-tag, referring to the image in your cache folder 
+		return "<img src=\"$cache_equation_url\" align=\"absmiddle\" class=\"tex\" alt=\"".($equation_text)."\" />";
+    }  
+}
+
+$geq_object = new geq;
+// Equations in content an excerpt should be parsed.
+add_filter('the_content', array($geq_object, 'parseTex'), 1);
+add_filter('the_excerpt', array($geq_object, 'parseTex'), 1);
+
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Google Equation for WordPress ===
+Contributors: Li Hao
+Donate link: 
+Tags: Google chart API, mathematics, equations, latex
+Requires at least: 2.3
+Tested up to: 2.5
+Stable tag: 0.1
+
+Using Google chart API for LaTeX to add mathematical equations as images.
+
+== Description ==
+
+This plugin can let you add mathematical equations to your posts using latex.
+Google chart API is used in this plugin to turn the latex strings to equation images.
+The equation images will be stored on your site, so it will be efficient.
+
+To add equations in your articles, just surround the latex script with '[eq]' and '[/eq]' tags.
+For example, add following codes '[eq]\alpha+\beta\geq\gamma[/eq]' in your posts.
+
+
+== Installation ==
+= Installation instruction =
+
+1. Upload `geq4wp` fold to the `/wp-content/plugins/` directory
+1. Activate the plugin `Google Equation for WordPress` through the 'Plugins' menu in WordPress
+1. done
+
+= uninstall instruction =
+
+1. deactivate the plugin or remove the plugin files.
+
+== Changelog ==
+
+= 0.1.0 =
+* The original version.
